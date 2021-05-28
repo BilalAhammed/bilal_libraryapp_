b45230608c6d3a384a128043cdda02ee08437724
Bilal Ahammed <82452006+BilalAhammed@users.noreply.github.com>
2021-05-28 23:53:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const navSU = require('./src/routes/signupRoute');//returning from signup router
 const navLN = require('./src/routes/loginRoute');//returning from login router page
 
 const app = express();
-const port = process.env.port || 2000;
+const PORT = process.env.PORT || 2000;
 
 const nav = [
     {
